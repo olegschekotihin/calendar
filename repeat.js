d436d80a720c0615dc2d0af498dc9b805b3cd44c
o.schekotihin <o.schekotihin@gmail.com>
2019-11-05 00:26:49 +0300

    working for repeat event

--- repeat.js
+++ repeat.js
@@ -4,10 +4,24 @@ var Repeat = (function (Calendar) {
     var NOT_CORRECT_ID = 'Enter the correct id';
 
     Calendar.repeatEventEveryday = function(id) {
+        var day = 86400000;
 
+
+        var repeatEventId = Calendar.findById(id);
+
+        repeatEventId.forEach(function (event) {
+            var eventNewRepeatDate = new Date(event.eventDate.getTime() + day);
+            event.done = false;
+            event.eventDate = eventNewRepeatDate;
+        });
     };
 
-    Calendar.createRepeatEvent = function() {
+    Calendar.callbackEventEveryDay = function () {
+      this.callback();
+      repeatEventEveryday();
+    };
+
+    Calendar.createRepeatEvent = function(id) {
         var currentDate = new Date();
         var day = 86400000;
 
@@ -26,33 +40,30 @@ var Repeat = (function (Calendar) {
                 console.log(event);
                 Calendar.removeEvent(event.id);
 
-
-                var eventNewRepeatDate = new Date(event.eventDate.getTime() + day);
-
-                return Calendar.createEvent(event.eventName, eventNewRepeatDate, event.callback);
+                return Calendar.createEvent(event.eventName, event.eventDate, Calendar.callbackEventEveryDay());
             }
         });
     };
 
-    Calendar.repeatEventByWeekDay = function (id, dayOfWeek) {
-        var repeatEventId = Calendar.findById(id);
-        var repeatDayOfWeek = dayOfWeek
-
-        if(!repeatEventId) {
-            return console.log(NOT_CORRECT_ID);
-        }
-
-        repeatEventId.forEach(function (event) {
-            if(event) {
-                console.log(event);
-                Calendar.removeEvent(event.id);
-
-                var eventNewRepeatDate = new Date(event.eventDate.getTime() + day);
-
-                return Calendar.createEvent(event.eventName, eventNewRepeatDate, event.callback);
-            }
-        });
-    };
+    // Calendar.repeatEventByWeekDay = function (id, dayOfWeek) {
+    //     var repeatEventId = Calendar.findById(id);
+    //     var repeatDayOfWeek = dayOfWeek
+    //
+    //     if(!repeatEventId) {
+    //         return console.log(NOT_CORRECT_ID);
+    //     }
+    //
+    //     repeatEventId.forEach(function (event) {
+    //         if(event) {
+    //             console.log(event);
+    //             Calendar.removeEvent(event.id);
+    //
+    //             var eventNewRepeatDate = new Date(event.eventDate.getTime() + day);
+    //
+    //             return Calendar.createEvent(event.eventName, eventNewRepeatDate, event.callback);
+    //         }
+    //     });
+    // };
 
     return Calendar;
 })(Calendar);
